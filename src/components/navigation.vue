572280bbce6301bb26dd8a5c38d6e678e62c9883
OF3493 <575558044@qq.com>
2021-04-12 15:17:32 +0800

    fix:修改导航样式

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -2,10 +2,39 @@
   <div class="nav-box">
     <!-- @select="handleSelect" -->
     <el-menu class="elm-box" mode="horizontal" router="true">
-      <el-menu-item index="/" class="elm-item">首页</el-menu-item>
-      <el-menu-item index="find" class="elm-item">发现</el-menu-item>
-      <el-menu-item index="order" class="elm-item">订单</el-menu-item>
-      <el-menu-item index="my" class="elm-item">我的</el-menu-item>
+      <el-menu-item index="/" class="elm-item">
+        <div class="icon-box">
+          <i
+            class="iconfont icon-shandian- icon-item"
+            style="font-size: 30px"
+          ></i>
+          <i class="text-sp">首页</i>
+        </div>
+      </el-menu-item>
+      <el-menu-item index="find" class="elm-item">
+        <div class="icon-box">
+          <i
+            class="iconfont icon-workstation icon-item"
+            style="font-size: 30px"
+          ></i>
+          <i class="text-sp">首页</i>
+        </div>
+      </el-menu-item>
+      <el-menu-item index="order" class="elm-item">
+        <div class="icon-box">
+          <i
+            class="iconfont icon-order-list icon-item"
+            style="font-size: 30px"
+          ></i>
+          <i class="text-sp">订单</i>
+        </div>
+      </el-menu-item>
+      <el-menu-item index="my" class="elm-item">
+        <div class="icon-box">
+          <i class="iconfont icon-person icon-item" style="font-size: 30px"></i>
+          <i class="text-sp">我的</i>
+        </div>
+      </el-menu-item>
     </el-menu>
   </div>
 </template>
@@ -29,7 +58,21 @@ export default defineComponent({
     .elm-item {
       display: flex;
       flex: 1;
+      height: 500px;
     }
+  }
+}
+.icon-box {
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  .icon-item {
+    width: 50px;
+    height: 50px;
+    color: aqua;
+  }
+  .text-sp {
+    margin-top: 1.1rem;
   }
 }
 </style>
