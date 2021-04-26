87e79aa0c76015c05483771b06d5841df3b0a884
OF3493 <575558044@qq.com>
2021-04-26 20:04:52 +0800

    feat: 小logo文件

---
+++ src/pages/homepage/little-logo-cate.vue
@@ -0,0 +1,28 @@
+<template></template>
+
+<script>
+import { defineComponent, reactive, toRefs } from "vue";
+export default defineComponent({
+  name: "LittleLogo",
+  props: {},
+  setup: () => {
+    const state = reactive({
+      //定义小logo
+      logos: [
+        {
+          url: "",
+          router: "",
+          name: "",
+        },
+      ],
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+});
+</script>
+
+<style>
+</style>
